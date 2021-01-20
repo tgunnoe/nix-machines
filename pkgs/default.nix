57ee09e0b7f80b59c2ae1c40881ac93cfb89ab6d
Taylor Gunnoe <t@gvno.net>
2021-01-20 02:51:12 -0500

    Add lzwolf to exported pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@ final: prev: {
   sddm-chili =
     prev.callPackage ./applications/display-managers/sddm/themes/chili { };
   dejavu_nerdfont = prev.callPackage ./data/fonts/dejavu-nerdfont { };
+  lzwolf = prev.callPackage ./games/lzwolf { };
   purs = prev.callPackage ./shells/zsh/purs { };
   pure = prev.callPackage ./shells/zsh/pure { };
   wii-u-gc-adapter = prev.callPackage ./misc/drivers/wii-u-gc-adapter { };
